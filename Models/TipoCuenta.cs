6fb659d0b4516459962d82b95680589035aab257
enmanuelCastillo5 <enmanuel.castillo@hotmail.com>
2024-09-24 18:32:41 -0400

    insertando TipoCuenta

--- Models/TipoCuenta.cs
+++ Models/TipoCuenta.cs
@@ -1,31 +1,30 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using ManejoPresupuesto.Validaciones;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
 
 namespace ManejoPresupuesto.Models
 {
-    public class TipoCuenta
+    public class TipoCuenta //: IValidatableObject
     {
         public int Id { get; set; }
         [Required(ErrorMessage ="El campo {0} es Requerido")]
-        [StringLength(maximumLength:50, MinimumLength =3, ErrorMessage = "La longitud del campo {0} debe estar {2} y {1}")]
-        [Display(Name = "Nombre del tipo cuenta")]
-        public string Nombre { get; set; }
+        [PrimeraLetraMayuscula]
+        [Remote(action: "VerificarExisteTipoCuenta", controller: "TiposCuentas")]
+        public string Nombre { get; set; }  
         public int UsuarioId { get; set; }
         public int Orden { get; set; }
 
-        /*pruebas de otras validaciones*/
-        [Required(ErrorMessage = "El campo {0} es Requerido")]
-        [EmailAddress(ErrorMessage = "El campo debe ser un correo electronico valido")]
-        public string Email { get; set;}
+      /*  public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if(Nombre !=  null && Nombre.Length > 0) {
+                var primeraLetra = Nombre[0].ToString();
 
-        [Range(minimum:18, maximum:130, ErrorMessage = "el valor debe estar entre {1} y {2}")]
-        public int Edad { get; set; }
-
-        [Url(ErrorMessage = "el campo debe ser una Url valida")]
-        public string URL { get; set; }
-
-
-        [CreditCard(ErrorMessage = "La tarjeta de credito no es valida")]
-        [Display(Name = "Tarjeta de credito")]
-        public int TarjetaDeCredito { get; set; }
+                if(primeraLetra != primeraLetra.ToUpper())
+                {
+                    yield return new ValidationResult("La primera letra debe estar en mayuscula"
+                        , new[] {nameof(Nombre)});
+                }
+            }
+        }*/
     }
 }
