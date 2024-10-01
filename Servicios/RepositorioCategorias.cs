edd7a2683e68a57a9240f3152206e3ef2f979139
enmanuelCastillo5 <enmanuel.castillo@hotmail.com>
2024-10-01 18:11:24 -0400

    Borrando Categorias

--- Servicios/RepositorioCategorias.cs
+++ Servicios/RepositorioCategorias.cs
@@ -7,6 +7,7 @@ namespace ManejoPresupuesto.Servicios
     public interface IRepositorioCategorias
     {
         Task Actualizar(Categoria categoria);
+        Task Borrar(int id);
         Task Crear(Categoria categoria);
         Task<IEnumerable<Categoria>> Obtener(int usuarioId);
         Task<Categoria> ObtenerPorId(int id, int usuarioId);
@@ -49,6 +50,12 @@ namespace ManejoPresupuesto.Servicios
             using var connection = new SqlConnection(connectionString);
             await connection.ExecuteAsync(@"UPDATE CategoriasTabla SET Nombre = @Nombre, TipoOperacionId = @TipoOperacionId
                                          WHERE Id = @Id", categoria);
+        }
+
+        public async Task Borrar(int id)
+        {
+            using var connection = new SqlConnection(connectionString);
+            await connection.ExecuteAsync("DELETE CategoriasTabla WHERE Id = @Id", new { id });
         }
     }
 }
