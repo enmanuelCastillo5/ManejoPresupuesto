574bca4276f7f7ba60045c7835cf338f787b6909
enmanuelCastillo5 <enmanuel.castillo@hotmail.com>
2024-10-01 13:24:14 -0400

    AutoMapper

---
+++ Servicios/AutoMapperProfiles.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using ManejoPresupuesto.Models;
+
+namespace ManejoPresupuesto.Servicios
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles() {
+            CreateMap<Cuenta, CuentaCreacionViewModel>();
+        }
+    }
+}
