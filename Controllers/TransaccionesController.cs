334f1e2c0570e41b1791ee81e43ab8b499648c77
enmanuelCastillo5 <enmanuel.castillo@hotmail.com>
2024-10-02 18:16:40 -0400

    creando transaccion

---
+++ Controllers/TransaccionesController.cs
@@ -0,0 +1,33 @@
+﻿using ManejoPresupuesto.Models;
+using ManejoPresupuesto.Servicios;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ManejoPresupuesto.Controllers
+{
+    public class TransaccionesController : Controller
+    {
+        private readonly IServicioUsuarios servicioUsuarios;
+        private readonly IRepositorioCuentas repositorioCuentas;
+
+        public TransaccionesController(IServicioUsuarios servicioUsuarios, IRepositorioCuentas repositorioCuentas)
+        {
+            this.servicioUsuarios = servicioUsuarios;
+            this.repositorioCuentas = repositorioCuentas;
+        }   
+
+        public async Task<ActionResult> Crear()
+        {
+            var usuarioId = servicioUsuarios.ObtenerUsuarioId();
+            var modelo = new TransaccionCreacionViewModel();
+            modelo.Cuentas = await ObtenerCuenta(usuarioId);
+            return View(modelo);
+        }
+
+        private async Task<IEnumerable<SelectListItem>> ObtenerCuenta(int usuarioId)
+        {
+            var cuentas = await repositorioCuentas.Buscar(usuarioId);
+            return cuentas.Select(x => new SelectListItem(x.Nombre, x.Id.ToString()));
+        }
+    }
+}
