34f6f0c2200defe878f1327abb556947bc441acd
enmanuelCastillo5 <enmanuel.castillo@hotmail.com>
2024-12-12 17:57:47 -0400

    DropDown Cascada - TipoOperacion

--- Models/Transaccion.cs
+++ Models/Transaccion.cs
@@ -5,17 +5,27 @@ namespace ManejoPresupuesto.Models
     public class Transaccion
     {
         public int Id { get; set; }
+
+
         public int UsuarioId { get; set; }
+
+        [Display(Name = "Fecha Transaccion")]
+        [DataType(DataType.Date)]
         public DateTime FechaTransaccion { get; set; } = DateTime.Today;
+        
+        
         public decimal Monto { get; set; }
 
+        
         [Range(1, maximum:int.MaxValue, ErrorMessage ="Debes seleccionar una categoria")]
+        [Display(Name ="Categoria")]
         public int CategoriaId { get; set; }
 
         [StringLength(maximumLength:1000, ErrorMessage ="La nota no puede pasar de {1} caracteres")]
         public string Nota { get; set; }
 
 
+        [Display(Name = "Cuenta")]
         [Range(1, maximum: int.MaxValue, ErrorMessage = "Debes seleccionar una cuenta")]
         public int CuentaId { get; set; }  
      }
--- Models/TransaccionCreacionViewModel.cs
+++ Models/TransaccionCreacionViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
 
 namespace ManejoPresupuesto.Models
 {
@@ -6,5 +7,8 @@ namespace ManejoPresupuesto.Models
     {
         public IEnumerable<SelectListItem> Cuentas { get; set; }
         public IEnumerable<SelectListItem> Categorias { get; set; }
+
+        [Display(Name = "Tipo Operacion")]
+        public TipoOperacion TipoOperacionId { get; set; } = TipoOperacion.Ingreso;
     }
 }
